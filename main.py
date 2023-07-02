cf0442678fb612bdcf246bf34037d7ae5672b037
minyuecmhe <minyuecmhe@tencent.com>
2023-07-02 17:01:38 +0800

    feat(porcupine): path

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import pvporcupine
+PICOVOIVCE_API_KEY = "71Ux51hT6FvYKa4fNS02K7OkgFuqhG1CyBtVQnTl7WSq1YSqXTnzxQ=="
+porcupine = pvporcupine.create(
+    access_key= PICOVOIVCE_API_KEY,
+    keyword_paths=['/Users/minyue/Desktop/minyue/Orator/assets/hei-cherry_en_mac_v2_2_0/hei-cherry_en_mac_v2_2_0.ppn']
+)
+
+#pyaudio (用电脑的麦克风来录入声音)
